bbdd93869b611da9156a6b10ed04acc87cf9be1b
FernaandoJr <fernaando.divino@gmail.com>
2026-02-18 18:47:29 -0300

    chore: update Tailwind CSS dependencies and refactor styles in the web app

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,37 +1,7 @@
-import { Button } from "@repo/ui/button"
-import Image from "next/image"
-
 export default function Home() {
 	return (
 		<div className="flex flex-col items-center justify-center min-h-screen">
-			<main className="flex flex-col gap-4">
-				<Image
-					className="w-10 h-10"
-					src="/logo.svg"
-					alt="Logo"
-					width={120}
-					height={32}
-					priority
-				/>
-				<ol>
-					<li>
-						Get started by editing{" "}
-						<code>apps/web/app/page.tsx</code>
-					</li>
-					<li>Save and see your changes instantly.</li>
-				</ol>
-				<Button appName="web" className="bg-blue-500 text-white">
-					Open alert
-				</Button>
-			</main>
-			<footer className="text-sm text-gray-500">
-				<a
-					href="https://turborepo.dev?utm_source=create-turbo"
-					target="_blank"
-					rel="noopener noreferrer">
-					Go to turborepo.dev →
-				</a>
-			</footer>
+			<h1 className="text-4xl font-bold">Hello World</h1>
 		</div>
 	)
 }
